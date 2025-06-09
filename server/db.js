a0260fc0684dc41814bbe9642bf3ac12dfcc6062
Gabriel Li <li.gabriel.h@gmail.com>
2025-06-09 13:00:50 -0400

    Added DB connection

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:ZDPuA8CGanM9VNrXhGIRGNB7@172.21.126.64:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
