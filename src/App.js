75fbeb8cf1cdc472e2ffb87a1f12ffe26ee38dd9
Gabriel Li <li.gabriel.h@gmail.com>
2025-06-09 18:40:49 -0400

    Added Doctor Search

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import LandingPage from './Components/Landing_Page/LandingPage';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 // Function component for the main App
 function App() {
@@ -29,6 +30,7 @@ function App() {
             <Route path="/Sign_Up" element={<Sign_Up/>}/>
             <Route path="/Login" element={<Login/>}/>
             <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="/search/doctors" element={<FindDoctorSearch/>}/>
           </Routes>
         </BrowserRouter>
     </div>
